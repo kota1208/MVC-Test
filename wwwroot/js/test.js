1b0eb9067ef5b143675d040bba23735482e7863d
kota1208 <kouta.nakane2000@gmail.com>
2022-06-13 11:13:19 +0900

    11:07

--- wwwroot/js/test.js
+++ wwwroot/js/test.js
@@ -1,2 +1,13 @@
 ﻿console.log("Piyo");
 
+function checkRangeAge() {
+    let result = window.confirm("送信しますか");
+    if (result == true) {
+        document.inputForm.submit();
+    } else {
+
+    }
+    
+    let name = document.getElementById("name");
+    alert("名前->" + name.value);
+}
