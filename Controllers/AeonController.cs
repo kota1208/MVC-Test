c9ea54ee4260ff9e677f1fbf777af786a328b5f0
kota1208 <kouta.nakane2000@gmail.com>
2022-07-10 15:38:09 +0900

    ファーストコミット

---
+++ Controllers/AeonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Test.Controllers
+{
+    public class AeonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Views/Aeon/Index.cshtml");
+        }
+    }
+}
